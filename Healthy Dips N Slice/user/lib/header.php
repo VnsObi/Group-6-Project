6a81bedea13836d930ea599113a29f6a8bdea53e
Evans Obi <32423919+VnsObi@users.noreply.github.com>
2020-11-13 18:35:44 +0100

    Add files via upload

---
+++ Healthy Dips N Slice/user/lib/header.php
@@ -0,0 +1,28 @@
+<?php
+echo <<<EOT
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="utf-8">
+                
+                <link href="main.css" rel="stylesheet" type="text/css">
+                <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+            </head>
+            <body>
+                <header>
+                    <div class="content-wrapper">
+                        <h1>Shopping Cart System</h1>
+                        <nav>
+                            <a href="index.php">Home</a>
+                            <a href="dashboard.php?page=products">Products</a>
+                        </nav>
+                        <div class="link-icons">
+                            <a href="index.php?page=cart">
+                                <i class="fas fa-shopping-cart"></i>
+                               
+                            </a>
+                        </div>
+                    </div>
+                </header>
+        EOT;
+?>
