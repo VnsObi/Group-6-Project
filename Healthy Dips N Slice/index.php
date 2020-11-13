2d1be49f4fc4adfa7f7a2d605be8b0bacc21c38f
Evans Obi <32423919+VnsObi@users.noreply.github.com>
2020-11-13 17:28:19 +0100

    Group 6 project Work

---
+++ Healthy Dips N Slice/index.php
@@ -0,0 +1,362 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Welcome to Healthy Dips N Slice</title>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Unicat project">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="assets/bootstrap4/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="assets/OwlCarousel2-2.2.1/owl.carousel.css">
+<link rel="stylesheet" type="text/css" href="assets/OwlCarousel2-2.2.1/owl.theme.default.css">
+<link rel="stylesheet" type="text/css" href="assets/OwlCarousel2-2.2.1/animate.css">
+<link rel="stylesheet" type="text/css" href="assets/css/main_styles.css">
+<link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+</head>
+<body>
+<?php include 'lib/header.php'; ?>
+<div class="super_container">
+
+	<!-- Header -->
+
+	<header class=>
+			
+			<!-- Top Bar -->
+			<div class="top_bar">
+			<div class="top_bar_container">
+				<div class="container">
+					<div class="row">
+						<div class="col">
+							<div class="top_bar_content d-flex flex-row align-items-center justify-content-start">
+								<ul class="top_bar_contact_list">
+									<li><div class="question">Have any questions?</div></li>
+									<li>
+										<i class="fa fa-phone" aria-hidden="true"></i>
+										<div>(+234)7064910016</div>
+									</li>
+									<li>
+										<i class="fa fa-envelope-o" aria-hidden="true"></i>
+										<div>vnsobi@gmail.com</div>
+									</li>
+								</ul>
+								<div class="top_bar_login ml-auto">
+                                   
+                                    <div class="login_button"><a href="admin/index.php">Admin Login</a></div>
+                                  
+                </div>
+                <div class="top_bar_login ">
+                                    <div class="login_button"><a href="user/login.php">Customer Login</a></div>
+                                   
+                </div>
+                <div class="top_bar_login">
+                                
+                                    <div class="login_button"><a href="blog/index.php">Blog</a></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>				
+        </div>
+
+
+	
+	<!-- Home -->
+
+	<div class="home">
+		<div class="home_slider_container">
+			
+			<!-- Home Slider -->
+			<div class="owl-carousel owl-theme home_slider">
+				
+				<!-- Home Slider Item -->
+				<div class="owl-item">
+					<div class="home_slider_background" style="background-image:url(images/home_slider_1.jpg)"></div>
+    
+				</div>
+
+				<!-- Home Slider Item -->
+				<div class="owl-item">
+					<div class="home_slider_background" style="background-image:url(images/home_slider_2.jpg)"></div>
+					
+				</div>
+
+				
+
+			</div>
+		</div>
+
+		<!-- Home Slider Nav -->
+
+		<div class="home_slider_nav home_slider_prev"><i class="fa fa-angle-left" aria-hidden="true"></i></div>
+		<div class="home_slider_nav home_slider_next"><i class="fa fa-angle-right" aria-hidden="true"></i></div>
+		
+	
+	
+	<!-- Our Services -->
+	
+	
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="section_title_container text-center">
+						<h2 class="section_title">Welcome To Healthy Dips<br/> N Slice</h2><br/>
+						<div class="section_subtitle"><p> To provide healthy, delicious seasonal diet and a fun atmosphere
+                        hence providing optimal nutrition and maintaining a healthy
+                                                         lifestyle for customers.</p></div><br/><br/>
+					</div>
+				</div>
+			</div>
+			<div class="row Our Services_row">
+				
+				<div align ='center'>
+				<div class="col-lg-4 feI. C. Ture_col" >
+					<div class="feI. C. Ture text-center trans_400">
+						<h3 class="feI. C. Ture_title">MISSION</h3>
+						<div class="feI. C. Ture_text"><p>Healthy dip n slice brand aims at giving our customers 
+                        healthy meals and be their favorite place to eat and drink.</p></div>
+					</div>
+				</div>
+                </div>
+				
+
+			</div>
+		</div>
+	</div>
+	<br/>
+	<br/>
+	<br/>
+		<!-- Counter -->
+
+	<div class="counter">
+		<div class="counter_background" style="background-image:url(images/background.jpg)"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="counter_content">
+						<h2 class="counter_title">Book Now</h2>
+						<div class="counter_text"><p>To provide healthy, delicious seasonal diet and a fun atmosphere
+                        hence providing optimal nutrition and maintaining a healthy
+                                                         lifestyle for customers.</p></div>
+
+					</div>
+
+				</div>
+			</div>
+
+			<div class="counter_form">
+				<div class="row fill_height">
+					<div class="col fill_height">
+						<form class="counter_form_content d-flex flex-column align-items-center justify-content-center" action="#">
+							<div class="counter_form_title">Book now</div>
+							<input type="text" class="counter_input" placeholder="Your Name:" required="required">
+							<input type="text" class="counter_input" placeholder="E-mail:" required="required">
+							<input type="tel" class="counter_input" placeholder="Phone:" required="required">
+							<select name="counter_select" id="counter_select" class="counter_input counter_options">
+								<option>Appetizer</option>
+								<option>Main Dish</option>
+								<option>African</option>
+								<option>Wine</option>
+								<option>Side Meal</option>
+								<option>Dip N Slice Cream Bar</option>
+								<option>More...</option>
+							</select>
+							<textarea class="counter_input counter_text_input" placeholder="More info on your Dish:" required="required"></textarea>
+                            <a href="user/login.php">
+							<button type="submit" class="counter_form_button">submit now</button>
+                            </a>
+						</form>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<!-- Popular Programs -->
+
+	<div class="Programs">
+		
+		<div class="container">
+			<div class="row">
+				<div class="col">
+					<div class="section_title_container text-center">
+						<h2 class="section_title">What we Offer</h2>
+						<div class="section_subtitle"><p>Below are some of our popular dishes at our for you.</p></div>
+					</div>
+				</div>
+			</div>
+			<div class="row Programs_row">
+				
+				<!-- Course -->
+				<div class="col-lg-4 course_col">
+					<div class="course">
+						<div class="course_image"><img src="images/course_1.png" alt=""></div>
+						<div class="course_body">
+							<h3 class="course_title"><a href="course.html">Quality Food</a></h3>
+							<div class="course_text">
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem possimus temporibus doloribus earum id, est qui vel nostrum ipsa placeat ducimus quos assumenda cumque quia perferendis ipsum labore asperiores ab.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Course -->
+				<div class="col-lg-4 course_col">
+					<div class="course">
+						<div class="course_image"><img src="images/course_2.png" alt=""></div>
+						<div class="course_body">
+							<h3 class="course_title"><a href="course.html">Quality Food</a></h3>
+							<div class="course_text">
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem possimus temporibus doloribus earum id, est qui vel nostrum ipsa placeat ducimus quos assumenda cumque quia perferendis ipsum labore asperiores ab.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- Course -->
+				<div class="col-lg-4 course_col">
+					<div class="course">
+						<div class="course_image"><img src="images/course_3.png" alt=""></div>
+						<div class="course_body">
+							<h3 class="course_title"><a href="course.html">Quality Food</a></h3>
+							<div class="course_text">
+								<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem possimus temporibus doloribus earum id, est qui vel nostrum ipsa placeat ducimus quos assumenda cumque quia perferendis ipsum labore asperiores ab.</p>
+							</div>
+						</div>
+						
+					</div>
+				</div>
+
+			</div>
+			<div class="row">
+				<div class="col">
+					<div class="Programs_button trans_200"><a href="dashboard.php">View Menu</a></div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+	<!-- Footer -->
+
+	<footer class="footer">
+		<div class="footer_background" style="background-image:url(images/footer_background.png)"></div>
+		<div class="container">
+			<div class="row footer_row">
+				<div class="col">
+					<div class="footer_content">
+						<div class="row">
+
+							<div class="col-lg-3 footer_col">
+					
+								<!-- Footer About -->
+								<div class="footer_section footer_about">
+									<div class="footer_logo_container">
+										<a href="#">
+											<div class="footer_logo_text">Healthy Dip <span>N Slice</span></div>
+										</a>
+									</div>
+									<div class="footer_about_text">
+										<p>Got a social media account? Follow us to catch up with the new dishes.</p>
+									</div>
+									<div class="footer_social">
+										<ul>
+											<li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+											<li><a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+											<li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+											<li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+										</ul>
+									</div>
+								</div>
+								
+							</div>
+
+							<div class="col-lg-3 footer_col">
+					
+								<!-- Footer Contact -->
+								<div class="footer_section footer_contact">
+									<div class="footer_title">Find Us</div>
+									<div class="footer_contact_info">
+										<ul>
+											<li>Address: 123 nulla a cursus rhoncus,<br> augue sem viverra 10870<br>id ultricies sapien</li>
+											
+										</ul>
+									</div>
+								</div>
+								
+							</div>
+
+							<div class="col-lg-3 footer_col">
+					
+								<!-- Footer links -->
+								<div class="footer_section footer_links">
+									<div class="footer_title">Reservation</div>
+									<div class="footer_links_container">
+										<ul>
+                                        <p>090-080-0650 | 090-070-0430</p>
+                                    <p><a href="mailto:info@company.com">info@company.com</a></p>
+                                    <p>LINE: eatery247 </p> 
+										</ul>
+									</div>
+								</div>
+								
+							</div>
+
+                            <div class="col-lg-3 footer_col">
+					
+								<!-- Footer links -->
+								<div class="footer_section footer_links">
+									<div class="footer_title">Open Hours</div>
+									<div class="footer_links_container">
+										<ul>
+                                        <p>Monday to Friday</p>
+                                   
+                                    <p>Saturday to Sunday </p> 
+                                    <p>7:00 AM - 9:00 PM</p>
+                                    <p>11:00 AM -  10:00 PM</p>
+										</ul>
+									</div>
+								</div>
+								
+							</div>
+
+							<div class="col-lg-3 footer_col clearfix">			
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row copyright_row">
+				<div class="col">
+					<div class="copyright d-flex flex-lg-row flex-column align-items-center justify-content-start">
+						<div class="cr_text"><!-- Link back to Colorlib can't be removed. TemplI. C. Te is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new DI. C. Te().getFullYear());</script> All rights reserved | </a>
+<!-- Link back to Colorlib can't be removed. TemplI. C. Te is licensed under CC BY 3.0. --></div>
+						<div class="ml-lg-auto cr_links">
+							<ul class="cr_list">
+								<li><a href="#">Copyright notification</a></li>
+								<li><a href="#">Terms of Use</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</footer>
+</div>
+<?php include 'lib/footer.php'; ?>
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/bootstrap4/popper.js"></script>
+<script src="assets/bootstrap4/bootstrap.min.js"></script>
+<script src="assets/scrollmagic/ScrollMagic.min.js"></script>
+<script src="assets/OwlCarousel2-2.2.1/owl.carousel.js"></script>
+<script src="assets/js/custom.js"></script>
+</body>
+</html>
